ab49ee83b024a25b900ccbd7bf6dc62687873567
mparje <126131806+mparje@users.noreply.github.com>
2023-05-21 07:40:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-api_key = os.getenv("OPENAI_API_KEY")
+
 
 def main():
-    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF 💬")
     
